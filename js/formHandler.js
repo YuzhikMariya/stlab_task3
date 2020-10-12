9472444f924639f2a3fd92e203c8a2e4a8488058
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-10-12 09:57:18 +0300

    added module for helper functions

--- js/formHandler.js
+++ js/formHandler.js
@@ -78,33 +78,3 @@ function getCheckedCheckBoxes(className) {
     return checkboxesChecked;
 } 
 
-function isObjExist(date, arr){
-    let index = -1;
-   for(let i = 0; i < arr.length; i++){
-       if(arr[i]._date == date){
-           index = i;
-       }
-   }
-
-    return index;
-}
-
-function getDate(str){
-    let date = new Date(str);
-    let day = date.getDate();
-    let month = date.getMonth()+1;
-    let year = date.getFullYear();
-
-    let resultStr = year + "-";
-    if(month < 10){
-        resultStr += '0';
-    }
-    resultStr += month + "-";
-    if(day < 10){
-        resultStr += '0';
-    }
-    resultStr += day;
-
-
-    return  resultStr;
-}
