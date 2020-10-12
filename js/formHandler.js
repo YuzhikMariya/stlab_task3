269f6280c290216cca0529ccd1b4a8bead9de822
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-10-12 10:18:52 +0300

    added module for building and executing requests

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,7 +1,5 @@
 let form = document.querySelector('.currency_rate_form');
 
-let dataLoader = new DataLoader();
-
 form.onsubmit = () => {
 
 
@@ -16,54 +14,7 @@ form.onsubmit = () => {
         return false;
     }
     let currencyNames = getCheckedCheckBoxes('currency');
-
-
-
-    let resultArray = [];
-
-    let daysCount = (Date.parse(to) - Date.parse(from)) / (1000 * 60 * 60 * 24),
-        yearsCount = Math.ceil(daysCount / 365);
-    let processedYear = 0;
-    const MS_COUNT_IN_YEAR = 365*24*60*60*1000,
-          MS_COUNT_IN_DAY = 24*60*60*1000;
-    const CURR_OBJECT = {
-            "USD": 0,
-            "EUR": 0,
-            "PLN": 0,
-            "JPY": 0,
-            "RUB": 0,
-            "KZT": 0
-    };
-    for(let i = 0; i < currencyNames.length; i++) {
-
-        for(k = 0; k < yearsCount; k++){
-            let tempFrom = getDate(Date.parse(from)+k*MS_COUNT_IN_YEAR),
-                tempTo = getDate(Math.min(Date.parse(to), Date.parse(tempFrom)+MS_COUNT_IN_YEAR-MS_COUNT_IN_DAY));
-            dataLoader.getJSON(tempFrom, tempTo, currencyNames[i])
-            .then(res => {
-
-                for(let j = 0; j < res.length; j++){
-
-                    let index = isObjExist(res[j].Date, resultArray);
-                    if(index == -1){
-                        let tempObj = new Currency(res[j].Date, CURR_OBJECT);
-                        tempObj.addCurrency(currencyNames[i], res[j].Cur_OfficialRate);
-                        resultArray.push(tempObj);
-                    }else{
-                        resultArray[index].addCurrency(currencyNames[i], res[j].Cur_OfficialRate);
-                    }
-                    
-                }
-                processedYear++;
-                if(processedYear == yearsCount*currencyNames.length){
-                    section.innerHTML = '';
-                    section.appendChild(TableCreator.addTable(resultArray.sort(function(a, b){return Date.parse(a._date) - Date.parse(b._date)}), currencyNames));
-                }
-
-            })
-        }
-    };
-    
+    QueryBuilder.build(from, to, currencyNames, {section: section});
     return false;
 }
 
