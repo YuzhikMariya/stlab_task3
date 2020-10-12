45e1d72a8da143e4240450516c0c09cac3173564
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-10-12 09:53:37 +0300

    added table creator

--- js/formHandler.js
+++ js/formHandler.js
@@ -57,7 +57,7 @@ form.onsubmit = () => {
                 processedYear++;
                 if(processedYear == yearsCount*currencyNames.length){
                     section.innerHTML = '';
-                    section.appendChild(addTable(resultArray.sort(function(a, b){return Date.parse(a._date) - Date.parse(b._date)}), currencyNames));
+                    section.appendChild(TableCreator.addTable(resultArray.sort(function(a, b){return Date.parse(a._date) - Date.parse(b._date)}), currencyNames));
                 }
 
             })
@@ -66,46 +66,6 @@ form.onsubmit = () => {
     
     return false;
 }
-
-function addTable(rateArr, currNameArr){
-    let table = document.createElement('table');
-    table.setAttribute("border", "2px");
-
-    let tr = document.createElement('tr');
-    let th = document.createElement('th');
-
-    th.appendChild(document.createTextNode('Date'));
-    tr.appendChild(th);
-    for(let i = 0; i < currNameArr.length; i++){
-        th = document.createElement('th');
-        th.appendChild(document.createTextNode(currNameArr[i]));
-        tr.appendChild(th);
-    }
-
-    table.appendChild(tr);
-
-    for(let i = 0; i < rateArr.length; i++){
-        tr = document.createElement('tr');
-
-        let tdDate = document.createElement('td');
-        let date = document.createTextNode(getDate(rateArr[i]._date));
-        tdDate.appendChild(date);
-        tr.appendChild(tdDate);
-
-        for(let j = 0; j < currNameArr.length; j++){
-            let td = document.createElement('td');
-            let text = document.createTextNode(rateArr[i]._currObj[currNameArr[j]]);
-
-            td.appendChild(text);
-            tr.appendChild(td);
-        }
-
-        table.appendChild(tr);
-    }
-
-    return table;
-}
-
 
 function getCheckedCheckBoxes(className) {
     let checkboxes = document.getElementsByClassName(className);
